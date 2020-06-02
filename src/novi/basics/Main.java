e122d73bc996e7ebc7b1df476c49502ea4e406df
srwijnberg@gmail.com <sharonrosa@Sharons-MacBook-Pro.local>
2020-06-02 11:47:20 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello Sharon");
+        LoginController loginController = new LoginController();
+
     }
 }
